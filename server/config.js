9a7cea8f79bbb611bf23d462a9192e356552455e
sunyaqin <sunyaqin@ehousechina.com>
2018-08-05 20:38:20 +0800

    部分联调

---
+++ server/config.js
@@ -0,0 +1,13 @@
+const config = {
+  port: 3000,
+
+  database: {
+    DATABASE: 'node-koa',
+    USERNAME: '',
+    PASSWORD: '',
+    PORT: '27017',
+    HOST: '127.0.0.1'
+  }
+}
+
+module.exports = config
