5580a988852f46dacc67f76ea26613349d360f8d
sunyaqin <sunyaqin@ehousechina.com>
2018-08-12 12:18:28 +0800

    注册和登录接口完善

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,5 @@
 
-const User = require('../services/user');
+const User = require('../models/user');
 
 /**
 * 注册操作
@@ -9,20 +9,34 @@ const User = require('../services/user');
 const signIn = async ctx => {
   let result = {
     success: false,
-    message: '登录失败'
+    message: '注册失败'
   }
 
-  const formData = ctx.request.body;
-  const { username, password } = formData
-  
-  let userResult = await User.signIn(formData)
-  console.info(userResult, 'controllers---------------')
+  const { username, password } = ctx.request.body;
+  console.info(username, password, ctx)
 
-  if (!(username && password)) {
+  if (!username || !password) {
     result.message = '请输入用户名和密码';
     ctx.body = result;
   } else {
-    ctx.body = userResult
+    let user = await User.findOne({username});
+    if (!user) {
+      const uModel = new User({ username, password });
+      const doc = await uModel.save();
+      if (!doc.errors) {
+        ctx.body = {
+          success: true,
+          message: '注册成功'
+        }
+      } else {
+        ctx.body = result;
+      }
+    } else {
+      ctx.body = {
+        success: false,
+        message: '用户名已存在'
+      }
+    }
   }
 };
 
@@ -31,7 +45,29 @@ const signIn = async ctx => {
  * @param {object} ctx
  */
 const login = async ctx => {
-	
+  let result = {
+    success: false,
+    message: '登录失败'
+  }
+  const { username, password } = ctx.request.body;
+  await User.findOne({username}, (err, user) => {
+    if (err) {
+      throw err
+    }
+
+    if (!user) {
+      ctx.body = {
+        success: false,
+        message: '用户不存在'
+      }
+    } else {
+      if (password == user.password) {
+        ctx.body = { success: true, message: '登录成功' }
+      } else {
+        ctx.body = { success: false, message: '密码错误'}
+      }
+    }
+  })
 };
 
 /**
@@ -54,12 +90,5 @@ const getUserInfo = async ctx => {
 	ctx.body = result;
 };
 
-/**
- * 校验登录
- * @param {object} ctx
- */
-const validateLogin = async ctx => {
-	
-}
 
 module.exports = { login, signIn, getUserInfo }
--- server/models/user.js
+++ server/models/user.js
@@ -1,52 +1,17 @@
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
 
-// const UserSchema = new Schema({
-//   username: {
-//     type: String,
-//     unique: true,
-//     require: true
-//   },
-//   password: {
-//     type: String,
-//     require: true
-//   }
-// });
-
-// module.exports = mongoose.model('user', UserSchema)
-
-const dbUtils = require('../util/db-util');
-
-const user = {
-  /**
-   * 数据库创建用户
-   * @param { Object } model 用户数据模型
-   * @return { Object } mysql 执行结果
-   */
-  create: async model => {
-    let result = await dbUtils.insertData('user', model)
-    return result
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    require: true
   },
-
-  /**
-   * 根据用户名和密码查找用户
-   * @param { Object } options 用户名密码对象
-   * @return { Object | null } 查找结果
-  */
-
-  getUserByUsernameOrPassword: async options => {
-    console.info('get------------models')
-    let _sql = `SELECT * from user
-    where password='${options.password}' and name='${options.username}'
-    limit 1`
-    let result = await dbUtils.query(_sql)
-    if (Array.isArray(result) && result.length > 0) {
-      result = result[0]
-    } else {
-      result = null
-    }
-    return result
+  password: {
+    type: String,
+    require: true
   }
-}
+});
 
-module.exports = user
+module.exports = mongoose.model('User', UserSchema)
+
