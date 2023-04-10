4d43a87f9d7598ae696061c9be0889020e869eca
xiaokuoai <lovelysyq1991225@qq.com>
2023-04-10 09:18:55 +0800

    feat: other

--- vue-project/src/views/lesscode/project/page-index/index.vue
+++ vue-project/src/views/lesscode/project/page-index/index.vue
@@ -1,11 +1,13 @@
 <script setup lang='ts'>
-
+import pageList from './components/page-list/index.vue';
 </script>
 
 <template>
-  <div>
-111
-  </div>
+  <main class="lesscode-editor-page">
+    <div class="lesscode-editor-page-header">
+      <page-list />
+    </div>
+  </main>
 </template>
 
 <style lang='scss' scoped>
